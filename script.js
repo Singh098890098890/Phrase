c49f859ed9a5074f21658d9099cfac8926e7610a
Singh098890098890 <h.srawn@share.epsb.ca>
2024-12-20 11:55:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("part1").addEventListener("click", function() {
+    alert("A blessing in disguise.");
+});
+
+document.getElementById("part2").addEventListener("click", function() {
+    alert("A dime a dozen.");
+});
+
+document.getElementById("part3").addEventListener("click", function() {
+    alert("Better late than never.");
+});
+
+document.getElementById("part4").addEventListener("click", function() {
+    alert("No pain, no gain.");
+});
+
+document.getElementById("part5").addEventListener("click", function() {
+    alert("Time flies when you're having fun.");
+});
+
+document.getElementById("part6").addEventListener("click", function() {
+    alert("The early bird catches the worm.");
+});
+
+document.getElementById("part7").addEventListener("click", function() {
+    alert("Actions speak louder than words.");
+});
